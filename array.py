e5716a98a08568f9c1280fb819426d5c2c766ab7
ganga02 <bgan0209@gmail.com>
2026-05-26 07:11:43 +0530

    second

--- array.py
+++ array.py
@@ -10,33 +10,35 @@ while(True):
     print("\n8.Sort")
     print("\n9.Exit")
 
-choice = int(input("Enter the choice:"))
-if choice == 1:
-    value = int(input("Enter the value to insert: "))
-    arr.append(value)
-elif choice == 2:
-    print(arr)
-elif choice == 3:
-    value = int(input("Enter the value to delete: "))
-    if value in arr:
-        arr.remove(value)
+    choice = int(input("Enter the choice:"))
+    if choice == 1:
+        value = int(input("Enter the value to insert: "))
+        arr.append(value)
+    elif choice == 2:
+        print(arr)
+    elif choice == 3:
+        value = int(input("Enter the value to delete: "))
+        if value in arr:
+            arr.remove(value)
+        else:
+            print("Element not found")
+    elif choice == 4:
+        value = int(input("Enter the value to search: "))
+        if value in arr:
+            print("Value Found")
+        else:
+            print("Value not Found")
+    elif choice == 5:
+        print(min(arr))
+    elif choice == 6:
+        print(max(arr))
+    elif choice == 7:
+        arr.reverse()
+        print(arr)
+    elif choice == 8:
+        arr.sort()
+        print(arr)
+    elif choice == 9:
+        break
     else:
-        print("Element not found")
-elif choice == 4:
-    value = int(input("Enter the value to search: "))
-    if value in arr:
-        print("Value Found")
-    else:
-        print("Value not Found")
-elif choice == 5:
-    print(arr.min())
-elif choice == 6:
-    print(arr.max())
-elif choice == 7:
-    print(arr.reverse())
-elif choice == 8:
-    print(arr.sort())
-elif choice == 9:
-    break
-else:
-    print("Invalid choice")
+        print("Invalid choice")
